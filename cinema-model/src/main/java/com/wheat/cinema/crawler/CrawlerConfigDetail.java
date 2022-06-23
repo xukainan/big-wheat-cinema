a756dc507fb99d1fdd0ba0e7f54f1a2134d2bb5d
xukainan <836127990@qq.com>
2022-06-23 17:30:38 +0800

    model

---
+++ cinema-model/src/main/java/com/wheat/cinema/crawler/CrawlerConfigDetail.java
@@ -0,0 +1,30 @@
+package com.wheat.cinema.crawler;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@TableName("crawler_config_detail")
+public class CrawlerConfigDetail {
+    @TableId(value = "id", type = IdType.AUTO)
+    private int id;
+    @TableField("config_id")
+    private int configId;
+    @TableField("config_url")
+    private String configUrl;
+    @TableField("config_xpath")
+    private String configXpath;
+    @TableField("config_time")
+    private Date createTime;
+    @TableField("config_time")
+    private Date updateTime;
+    @TableField("is_delete")
+    private int isDelete;
+    @TableField("config_type_id")
+    private int configTypeId;
+}
---
+++ cinema-model/src/main/java/com/wheat/cinema/crawler/CrawlerConfigType.java
@@ -0,0 +1,16 @@
+package com.wheat.cinema.crawler;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("crawler_config_type")
+public class CrawlerConfigType {
+    @TableId(value = "id", type = IdType.AUTO)
+    private int id;
+    @TableField("type_name")
+    private String typeName;
+}
